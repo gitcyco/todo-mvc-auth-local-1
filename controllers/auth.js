7cab0e9a9aa6bdb9b55b11d5897fe7adfa314405
gitcyco <gitcyco@users.noreply.github.com>
2022-09-05 18:10:29 -0700

    Implement "Remember Me" button for sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,15 +3,17 @@ const validator = require("validator");
 const User = require("../models/User");
 
 exports.getLogin = (req, res) => {
+  console.log("AUTH getLogin: ", req.body);
   if (req.user) {
     return res.redirect("/tickets");
   }
-  res.render("login", {
+  res.render("index", {
     title: "Login",
   });
 };
 
 exports.postLogin = (req, res, next) => {
+  console.log("AUTH postLogin: ", req.body);
   const validationErrors = [];
   if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: "Please enter a valid email address." });
   if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: "Password cannot be blank." });
@@ -22,7 +24,7 @@ exports.postLogin = (req, res, next) => {
   }
   req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
 
-  passport.authenticate("local", (err, user, info) => {
+  const out = passport.authenticate("local", {}, (err, user, info) => {
     if (err) {
       return next(err);
     }
@@ -34,10 +36,19 @@ exports.postLogin = (req, res, next) => {
       if (err) {
         return next(err);
       }
+      if (req.body.rememberMe) {
+        req.session.cookie.expires = false;
+        // console.log("cookie:", req.session, req.sessionOptions);
+      } else {
+        req.session.cookie.maxAge = 60000 * 10; // 10 minute expiration
+        // console.log("cookie:", req.session, req.sessionOptions);
+      }
+
       req.flash("success", { msg: "Success! You are logged in." });
       res.redirect(req.session.returnTo || "/tickets");
     });
-  })(req, res, next);
+  });
+  out(req, res, next);
 };
 
 exports.logout = (req, res) => {
